5f2ac9f9533af0597ceae1c0ac9eec0fedf2b160
InfSein <infsein@foxmail.com>
2025-06-23 21:42:58 +0800

    提前初始时间

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 //https://www.freecodecamp.org/news/how-to-format-dates-in-javascript/
 
-let date_1 = new Date('06/23/2025 23:00:00'); //start date 06/23/2025 23:00:00 = sb
+let date_1 = new Date('06/22/2025 23:00:00'); //start date 06/22/2025 23:00:00 = cf
 let date_2 = new Date(); //today
 
 //days between today and the start date: days(date_1, date_2)
@@ -21,32 +21,32 @@ function myTimer() {
 let remainder = days(date_1, date_2)%4;
 
 const img = [];
-img[0]='./assets/sb.png';
-img[1]='./assets/cy.png';
-img[2]='./assets/zd.png';
-img[3]='./assets/cf.png';
+img[0]='./assets/cf.png';
+img[1]='./assets/sb.png';
+img[2]='./assets/cy.png';
+img[3]='./assets/zd.png';
 
 function calculate(){
     if (remainder == 0){
-        document.title="今天什么战场？碎冰", 
-        document.getElementById("mainImage").src = img[0],
-        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是碎冰啦！",
-        document.getElementById("zcrotation").innerHTML = "<b>碎冰</b>>草原>阵地>尘封"};
-    if (remainder == 1){
-        document.title="今天什么战场？草原", 
-        document.getElementById("mainImage").src = img[1],
-        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是草原啦！",
-        document.getElementById("zcrotation").innerHTML = "碎冰><b>草原</b>>阵地>尘封"};
-    if (remainder == 2){
-        document.title="今天什么战场？阵地", 
-        document.getElementById("mainImage").src = img[2],
-        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是阵地啦！",
-        document.getElementById("zcrotation").innerHTML = "碎冰>草原><b>阵地</b>>尘封"};
-    if (remainder == 3){
         document.title="今天什么战场？尘封", 
-        document.getElementById("mainImage").src = img[3],
+        document.getElementById("mainImage").src = img[0],
         document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是尘封啦！",
         document.getElementById("zcrotation").innerHTML = "碎冰>草原>阵地<b>尘封</b>"};
+    if (remainder == 1){
+        document.title="今天什么战场？碎冰", 
+        document.getElementById("mainImage").src = img[1],
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是碎冰啦！",
+        document.getElementById("zcrotation").innerHTML = "<b>碎冰</b>>草原>阵地>尘封"};
+    if (remainder == 2){
+        document.title="今天什么战场？草原", 
+        document.getElementById("mainImage").src = img[2],
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是草原啦！",
+        document.getElementById("zcrotation").innerHTML = "碎冰><b>草原</b>>阵地>尘封"};
+    if (remainder == 3){
+        document.title="今天什么战场？阵地", 
+        document.getElementById("mainImage").src = img[3],
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是阵地啦！",
+        document.getElementById("zcrotation").innerHTML = "碎冰>草原><b>阵地</b>>尘封"};
 
 }
 calculate();
